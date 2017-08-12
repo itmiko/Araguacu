44b1d079b631419928804fe83f61b0eb83aef4e4
itmiko <mypost.mk@gmail.com>
2017-08-13 01:37:22 +0300

    first commit

--- app/js/common.js
+++ app/js/common.js
@@ -1,5 +1,34 @@
 $(function() {
 
 	// Custom JS
-
+	$(".hamburger").click(function() {
+		$(this).addClass("is-active");
+		// $(this).removeClass("is-active");
+	});  
+	//Owl-carousel fotos
+	$(".fotos").owlCarousel({
+		autoplay: true,
+		autoplayTimeout: 4000,
+		loop: true,
+		smartSpeed: 700,
+		items: 5,
+		responsiveClass: true,
+		responsive: {
+			0: {
+				items: 1
+			},
+			505: {
+				items: 2
+			},
+			768: {
+				items: 3
+			},
+			992: {
+				items: 4
+			},
+			1200: {
+				items: 5
+			},
+		}
+	});
 });
